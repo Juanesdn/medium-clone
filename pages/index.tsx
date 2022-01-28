6581f43741b405a7bf352e5d092f69ed23e3bcc6
Juan De La Cruz <jedelacruzn@gmail.com>
2022-01-28 11:41:30 -0500

    changes Readme and adds Hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
+      <Hero />
     </div>
   )
 }
